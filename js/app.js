e40f107cce694297c4ec33b8ebb5f8903936d8af
Sami Suo-Heikki <Sami Suo-Heikki>
2016-01-08 09:40:04 +0200

    Vaadin grid fetch data from firebase

--- js/app.js
+++ js/app.js
@@ -1,39 +1,8 @@
 // The Web Components polyfill introduces a custom event we can
 // use to determine when the custom elements are ready to be used
 document.addEventListener("WebComponentsReady", function () {
-
-    // Configure vaadin-grid to show data
-
-    // Reference to the grid element
-    var grid = document.querySelector("vaadin-grid");
-
-    // Fetch some JSON data from a URL
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == XMLHttpRequest.DONE) {
-            if (xhr.status == 200) {
-                var json = JSON.parse(xhr.responseText);
-
-                // Use the returned data array directly as the data source
-                // (keeping all the data source items in the browser's memory)
-                grid.items = json.results;
-            }
-        }
-    };
-    xhr.open("GET", "http://api.randomuser.me/?results=100", true);
-    xhr.send();
-
-    // Add a renderer for the index column
-    grid.columns[0].renderer = function(cell) {
-        cell.element.innerHTML = cell.row.index;
-    };
-
-    // Add a renderer for the picture column
-    grid.columns[1].renderer = function(cell) {
-        cell.element.innerHTML = '<img src="' + cell.data + '" style="width: 24px;">';
-    };
+    updateReportGrid();
     distributionBarChange(5, 12, 123);
-    fireBaseTesting();
 });
 
 function clearInput(e) {
@@ -67,13 +36,26 @@ function distributionBarChange(closed, assigned, unassigned) {
     $distributionBarUnAssigned.innerHTML = unassigned;
 }
 
-function fireBaseTesting() {
-    var ref = new Firebase("https://vaadin-bugrap.firebaseio.com/project");
-    ref.on("value", function(snapshot) {
-        console.log(snapshot.val());
+function updateReportGrid() {
+
+    // Reference to the grid element
+    var grid = document.querySelector("vaadin-grid");
+
+    // Configure vaadin-grid to show data
+    var ref = new Firebase("https://vaadin-bugrap.firebaseio.com/report");
+    ref.on("value", function(response) {
+        grid.items = response.val();
+        console.log(grid.items);
+
+
     }, function (errorObject) {
         console.log("The read failed: " + errorObject.code);
-    })
+    });
+
+    // Add a renderer for the index column
+    grid.columns[0].renderer = function(cell) {
+        cell.element.innerHTML = cell.row.index;
+    };
 }
 
 Polymer({
